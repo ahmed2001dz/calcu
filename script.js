6c5f15893ca376a1a01b3d1eab054da22a3af58f
ahmed2001dz <pohdan8@gmail.com>
2025-01-10 16:51:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let display;
+
+display = document.getElementById('display');
+
+function appendtodisplay(input) {
+    display.value += input;
+}
+
+function cleardisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try{
+    display.value = eval(display.value)
+    }
+    catch (error){
+        display.value = 'قود'
+    }
+    
+}
+
